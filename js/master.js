e08af0bfb032bbb231819c7a9b3f2efb2d7cd1d4
yuanyuanwa <1820001685@qq.com>
2021-03-03 09:48:37 +0800

    60%

--- js/master.js
+++ js/master.js
@@ -2,12 +2,16 @@
 var span3 = document.getElementsByClassName('span3');
 var top2 = document.getElementsByClassName('top2');
 
-span3[0].onclick=function(){
-    top2[0].style.display='none'
+span3[0].onclick = function () {
+    top2[0].style.display = 'none'
 }
-span3[1].onclick=function(){
-    top2[1].style.display='none'
+span3[1].onclick = function () {
+    top2[1].style.display = 'none'
 }
+span3[2].onclick = function () {
+    top2[2].style.display = 'none'
+}
+
 
 var hotBtn = document.getElementsByClassName('hotBtn');
 var newBtn = document.getElementsByClassName('newBtn');
@@ -15,18 +19,231 @@ var hotBtnn = document.getElementsByClassName('hotBtnn');
 var newBtnn = document.getElementsByClassName('newBtnn');
 // var top2 = document.getElementsByClassName('top2');
 var conUl = document.getElementsByClassName('conUl');
-var con22=document.getElementsByClassName('con22');
-hotBtn [0].onclick=function(){
-    conUl[0].style.display='block';
-    con22[0].style.display='none';
+var con22 = document.getElementsByClassName('con22');
+hotBtn[0].onclick = function () {
+    conUl[0].style.display = 'block';
+    con22[0].style.display = 'none';
     // top2[0].style.display='block'
-    hotBtnn[0].className='hotBtn';
-    newBtnn[0].className='newBtn';
+    hotBtnn[0].className = 'hotBtn';
+    newBtnn[0].className = 'newBtn';
 }
-newBtn[0].onclick=function(){
-    conUl[0].style.display='none';
-    con22[0].style.display='block';
+newBtn[0].onclick = function () {
+    conUl[0].style.display = 'none';
+    con22[0].style.display = 'block';
     // top2[0].style.display='none'
-    newBtn[0].className='newBtnn';
-    hotBtn[0].className='hotBtnn';
+    newBtn[0].className = 'newBtnn';
+    hotBtn[0].className = 'hotBtnn';
 }
+
+
+
+var hot = document.getElementsByClassName('hot');
+var hott = document.getElementsByClassName('hott');
+var t2 = document.getElementsByClassName('t2');
+var t3 = document.getElementsByClassName('t3');
+console.log(t3);
+var t4 = document.getElementsByClassName('t4');
+console.log(t4);
+var t5 = document.getElementsByClassName('t5');
+var t6 = document.getElementsByClassName('t6');
+var cont = document.querySelectorAll('.cont')
+console.log(1, cont)
+for (let i = 0; i < hot.length; i++) {
+    console.log(i)
+    hot[i].onclick = function () {
+        for (let j = 0; j < cont.length; j++) {
+            cont[j].style.display = 'none'
+            hot[j].style.border = "none"
+        }
+
+        hot[i].style.borderBottom = "2px solid #000"
+
+        cont[i].style.display = 'block'
+    }
+}
+// hot[0].onclick = function () {
+//     if (hott[0] == undefined) {
+//         hot[0].className = 'hott'
+//     } else {
+//         hott[0].className = 'hot';
+//         hot[0].className = 'hott'
+//         t2[0].style.display = 'block'
+//         t3[0].style.display = 'none'
+//         t4[0].style.display = 'none'
+//         t5[0].style.display = 'none'
+//         t6[0].style.display = 'none'
+//     }
+// }
+// hot[1].onclick = function () {
+//     if (hott[0] == undefined) {
+//         hot[1].className = 'hott';
+//     } else {
+//         hott[0].className = 'hot';
+//         console.log(1);
+//         hot[1].className = 'hott'
+//         t2[0].style.display = 'none'
+//         t3[0].style.display = 'block'
+//         t4[0].style.display = 'none'
+//         t5[0].style.display = 'none'
+//         t6[0].style.display = 'none'
+//     }
+// }
+// hot[2].onclick = function () {
+//     if (hott[0] == undefined) {
+//         hot[2].className = 'hott';
+//     } else {
+//         hott[0].className = 'hot';
+//         hot[2].className = 'hott'
+//         t2[0].style.display = 'none'
+//         t3[0].style.display = 'none'
+//         t4[0].style.display = 'block'
+//         t5[0].style.display = 'none'
+//         t6[0].style.display = 'none'
+//     }
+// }
+// hot[3].onclick = function () {
+//     if (hott[0] == undefined) {
+//         hot[3].className = 'hott';
+//     } else {
+//         hott[0].className = 'hot';
+//         hot[3].className = 'hott'
+//         t2[0].style.display = 'none'
+//         t3[0].style.display = 'none'
+//         t4[0].style.display = 'none'
+//         t5[0].style.display = 'block'
+//         t6[0].style.display = 'none'
+//     }
+// }
+// hot[4].onclick = function () {
+//     if (hott[0] == undefined) {
+//         hot[4].className = 'hott';
+//     } else {
+//         hott[0].className = 'hot';
+//         hot[4].className = 'hott'
+//         t2[0].style.display = 'none'
+//         t3[0].style.display = 'none'
+//         t4[0].style.display = 'none'
+//         t5[0].style.display = 'none'
+//         t6[0].style.display = 'block'
+//     }
+// }
+
+
+
+var zhankai = document.getElementsByClassName('zhankai');
+console.log(zhankai);
+var i = document.getElementsByClassName('i');
+var iC = document.getElementsByClassName('i');
+
+var iup = document.getElementsByClassName('iup')
+var top22 = document.getElementsByClassName('top22')
+for (let i = 0; i < zhankai.length; i++) {
+    console.log(top22)
+    let isExpand = false
+    zhankai[i].addEventListener('click', () => {
+        if (isExpand) {
+            // 收回
+
+            // top22[i].style.display = 'none'
+            // console.log(top22[i].style.display)
+            for (let k = 0; k < top22.length; k++) {
+
+                top22[k].style.display = 'none'
+
+                // top22[j].style.display = 'block' // 展开所有
+            }
+            isExpand = false
+
+        } else {
+            isExpand = true
+
+            for (let j = 0; j < top22.length; j++) {
+                // top22[j].style.display = 'none'
+                top22[j].style.display = 'block' // 展开所有
+
+            }
+            // top22[i].style.display = 'block' // 展开单个
+        }
+
+
+    })
+
+}
+// zhankai[0].onclick = function () {
+//     if (iup[0] == undefined) {
+//         i[0].className = 'iup';
+//         for (let i = 0; i < top22.length; i++) {
+//             top22[i].style.display = 'block'
+//         }
+//     } else {
+//         iup[0].className = 'i';
+//         for (let i = 0; i < top22.length; i++) {
+//             top22[i].style.display = 'none'
+//         }
+//     }
+// }
+
+
+// zhankai[1].onclick = function () {
+//     if (iup[0] == undefined) {
+//         i[1].className = 'iup';
+//         for (let i = 0; i < top22.length; i++) {
+//             top22[i].style.display = 'block'
+//         }
+//     } else {
+//         iup[0].className = 'i';
+//         for (let i = 0; i < top22.length; i++) {
+//             top22[i].style.display = 'none'
+//         }
+//     }
+
+// }
+// zhankai[2].onclick = function () {
+//     if (iup[0] == undefined) {
+//         i[2].className = 'iup';
+//         for (let i = 0; i < top22.length; i++) {
+//             top22[i].style.display = 'block'
+//         }
+//     } else {
+//         console.log(iup[0]);
+//         for (let i = 0; i < top22.length; i++) {
+//             top22[i].style.display = 'none'
+//         }
+//     }
+
+// }
+// zhankai[3].onclick = function () {
+//     if (iup[0] == undefined) {
+//         i[3].className = 'iup';
+//         for (let i = 0; i < top22.length; i++) {
+//             top22[i].style.display = 'block'
+//         }
+//     } else {
+//         iup[0].className = 'i';
+//         for (let i = 0; i < top22.length; i++) {
+//             top22[i].style.display = 'none'
+//         }
+//     }
+
+// }
+// zhankai[4].onclick = function () {
+//     if (iup[0] == undefined) {
+//         i[4].className = 'iup';
+//         for (let i = 0; i < top22.length; i++) {
+//             top22[i].style.display = 'block'
+//         }
+//     } else {
+//         iup[0].className = 'i';
+//         for (let i = 0; i < top22.length; i++) {
+//             top22[i].style.display = 'none'
+//         }
+//     }
+
+// }
+// console.log(document.nodeName);
+// zhankai[0].onclick=function(){
+//     console.log(1);
+//         iup[0].className='i';
+//         console.log(i[0]);
+
+// }
